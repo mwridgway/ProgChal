7c45e8f01600cb6a2ae371d8b3ab93d21e0e10f6
Matthew W Wridgway <matthew@Matthews-MacBook-Pro-2.local>
2014-11-05 14:08:11 +0200

    solved version

--- threenplusone.cpp
+++ threenplusone.cpp
@@ -1,45 +1,38 @@
-#include <iostream>
-using namespace std;
-
-//Start with an integer n. If n is even, divide by 2. If n is odd, multiply by 3 and add 1. Repeat this process with the new value of n, terminating when n = 1.
+#include<stdio.h>
 
 int countCycles(int n){
 	int cycleCount = 1;
-	//cout << "nums for " << n << " ";
 	while (n > 1) {
 		cycleCount++;
 		if(n % 2 == 1){
 			n = (n * 3) + 1;
 		}else{
-			n = n / 2;
+			n /= 2;
 		}
-		//cout << n << " ";
 	}
 	return cycleCount;
 }
 
-int main()
-{
-	int firstNum, secondNum, highestCycleCount;	
-	cout << "Enter two numbers: ";
-	cin >> firstNum >> secondNum;
-
-	cout << firstNum << " " << secondNum << " ";
-	//cout << "First " << firstNum << endl;
-	//cout << "Second " << secondNum << endl;
-	highestCycleCount = 0;	
-	for(int i = firstNum; i <= secondNum; i++){
+int maxBetween(int start, int end){
+	int highestCycleCount = 0;	
+	for(int i = start; i <= end; i++){
 		int cycles = countCycles(i);
 		if(cycles > highestCycleCount){
 			highestCycleCount = cycles;
 		}
-		//cout << "Cycles: " << cycles << endl;
-		//totalCycles += cycles;
 	}
-	
-	cout << highestCycleCount << endl;
+	return highestCycleCount;
+}
 
-	//cout << countCycles(firstNum) << endl;
-	//cout << countCycles(secondNum) << endl;
-	return 0;
+int main() { 
+	int firstNum, secondNum, highestCycleCount;	
+	while (scanf("%d %d",&firstNum,&secondNum) !=EOF) {
+
+		if(firstNum < secondNum){
+			highestCycleCount = maxBetween(firstNum, secondNum);
+		}else{
+			highestCycleCount = maxBetween(secondNum, firstNum);		
+		}
+		printf("%d %d %d\n", firstNum, secondNum, highestCycleCount);
+	}
 }
